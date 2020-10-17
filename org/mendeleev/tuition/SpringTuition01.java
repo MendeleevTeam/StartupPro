bdac1ddfe96692fdf4092cf7fe64b608b385c524
maxwellzs <maxwells.11942@gmail.com>
2020-10-17 23:56:30 +0800

    new Version

--- org/mendeleev/tuition/SpringTuition01.java
+++ org/mendeleev/tuition/SpringTuition01.java
@@ -1,12 +1,16 @@
 package org.mendeleev.tuition;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringTuition01 {
 
      public static void main(String[] args) {
-
+          AbstractApplicationContext context = new ClassPathXmlApplicationContext("NewBean.xml");
+          Object obj = context.getBean("MyBean01");
+          JavaBean01 bean = (JavaBean01)obj;
+          System.out.println(bean);
+          context.close();
      }
 
 }
